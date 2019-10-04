00187bf80dbcb2095e30315b72690d69c243c696
Marcin Chocilowicz <m.chocilowicz@gmail.com>
2019-10-04 22:29:03 +0200

    basic layoutaaa

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-    url: 'http://localhost:4000/api/dashboard/'
+    production: false,
+    // url: 'http://localhost:4000/api/dashboard/'
+    url: 'https://tadeus.herokuapp.com/api/dashboard/'
 };
 
 /*
