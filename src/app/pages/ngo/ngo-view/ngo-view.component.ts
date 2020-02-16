0ae1d4799c368aa0ade81a2bce140bf9627095f4
Marcin Chociłowicz <m.chocilowicz@gmail.com>
2020-02-16 11:07:03 +0100

    Dashboard - Ngo

--- src/app/pages/ngo/ngo-view/ngo-view.component.ts
+++ src/app/pages/ngo/ngo-view/ngo-view.component.ts
@@ -5,6 +5,7 @@ import {IType} from "../../../models/type.interface";
 import {ActivatedRoute, Router} from "@angular/router";
 import {CityService} from "../../../services/city.service";
 import {ICity} from "../../../models/city.interface";
+import {environment} from "../../../../environments/environment";
 
 @Component({
     selector: 'app-ngo-view',
@@ -20,9 +21,9 @@ export class NgoViewComponent implements OnInit {
     cities: ICity[] = null;
     ID: string = null;
 
-    icon: string = null;
+    image: string = null;
     thumbnail: string = null;
-    newIcon: File = null;
+    newImage: File = null;
     newThumbnail: File = null;
 
     constructor(private fb: FormBuilder,
@@ -37,30 +38,60 @@ export class NgoViewComponent implements OnInit {
         this.ID = id;
         if (id === 'new') {
             this.ngoForm = this.fb.group({
-                name: new FormControl('', [Validators.required]),
-                type: new FormControl('', [Validators.required]),
-                createdAt: new FormControl(new Date(), [Validators.required]),
-                bankAccount: new FormControl('', [Validators.required]),
-                phone: new FormControl('', [Validators.required]),
-                email: new FormControl('', [Validators.required, Validators.email]),
-                verified: new FormControl(false, [Validators.required]),
-                verifiedAt: new FormControl('', []),
-                collectedDonation: new FormControl(0, [Validators.required]),
+                name: new FormControl(null, [Validators.required]),
+                longName: new FormControl(null, [Validators.required]),
+                email: new FormControl(null, [Validators.required]),
+                bankNumber: new FormControl(null, [Validators.required]),
+                description: new FormControl(null, [Validators.required]),
+                verified: new FormControl(null, [Validators.required]),
+                verifiedAt: new FormControl({value: null, disabled: true}, []),
+                createdAt: new FormControl({
+                    value: new Date(),
+                    disabled: true
+                }, [Validators.required, Validators.email]),
+                type: new FormControl(false, [Validators.required]),
+                collectedDonation: new FormControl({value: 0, disabled: true}, []),
+                phone: new FormControl(null, [Validators.required]),
+                address: this.fb.group({
+                    street: new FormControl(null, [Validators.required]),
+                    number: new FormControl(null, [Validators.required]),
+                    postCode: new FormControl(null, [Validators.required]),
+                    latitude: new FormControl(null, [Validators.required]),
+                    longitude: new FormControl(null, [Validators.required]),
+                    city: new FormControl(null, [Validators.required])
+                })
             });
         } else {
             this.ngoService.getNgoBy(id).subscribe(r => {
                 if (r.data) {
+                    this.image = environment.url + 'img/' + r.data.image;
+                    this.thumbnail = environment.url + 'img/' + r.data.thumbnail;
+
                     this.transactions = r.data.transactions;
+
                     this.ngoForm = this.fb.group({
-                        name: new FormControl('', [Validators.required]),
-                        type: new FormControl('', [Validators.required]),
-                        createdAt: new FormControl(new Date(), [Validators.required]),
-                        bankAccount: new FormControl('', [Validators.required]),
-                        phone: new FormControl('', [Validators.required]),
-                        email: new FormControl('', [Validators.required, Validators.email]),
-                        verified: new FormControl(false, [Validators.required]),
-                        verifiedAt: new FormControl('', []),
-                        collectedDonation: new FormControl(0, [Validators.required]),
+                        name: new FormControl(r.data.name, [Validators.required]),
+                        longName: new FormControl(r.data.longName, [Validators.required]),
+                        email: new FormControl(r.data.email, [Validators.required, Validators.email]),
+                        bankNumber: new FormControl(r.data.bankNumber, [Validators.required]),
+                        description: new FormControl(r.data.description, [Validators.required]),
+                        verified: new FormControl(r.data.verified, [Validators.required]),
+                        verifiedAt: new FormControl({value: r.data.verifiedAt, disabled: true}, []),
+                        createdAt: new FormControl({
+                            value: new Date(r.data.createdAt),
+                            disabled: true
+                        }, [Validators.required]),
+                        type: new FormControl(r.data.type, [Validators.required]),
+                        collectedDonation: new FormControl({value: r.data.collectedDonation, disabled: true}, []),
+                        phone: new FormControl(r.data.phone, [Validators.required]),
+                        address: this.fb.group({
+                            street: new FormControl(r.data.address.street, [Validators.required]),
+                            number: new FormControl(r.data.address.number, [Validators.required]),
+                            postCode: new FormControl(r.data.address.postCode, [Validators.required]),
+                            latitude: new FormControl(r.data.address.latitude, [Validators.required]),
+                            longitude: new FormControl(r.data.address.longitude, [Validators.required]),
+                            city: new FormControl(r.data.address.city, [Validators.required])
+                        })
                     });
                 } else {
                     this.router.navigateByUrl('ngo')
@@ -79,8 +110,8 @@ export class NgoViewComponent implements OnInit {
     }
 
 
-    onIconChange($event) {
-        this.newIcon = $event.target.files[0]
+    onImageChange($event) {
+        this.newImage = $event.target.files[0]
     }
 
     onThumbnailChange($event) {
@@ -90,8 +121,8 @@ export class NgoViewComponent implements OnInit {
     updateImage() {
         if (this.canUploadImage()) {
             const form = new FormData();
-            if (this.newIcon) {
-                form.append('icon', this.newIcon);
+            if (this.newImage) {
+                form.append('image', this.newImage);
             }
             if (this.newThumbnail) {
                 form.append('thumbnail', this.newThumbnail);
@@ -101,6 +132,6 @@ export class NgoViewComponent implements OnInit {
     }
 
     canUploadImage() {
-        return this.ID && this.ID.length > 0 && this.ID !== 'new' && (this.newIcon !== null || this.newThumbnail !== null);
+        return this.ID && this.ID.length > 0 && this.ID !== 'new' && (this.newImage !== null || this.newThumbnail !== null);
     }
 }
