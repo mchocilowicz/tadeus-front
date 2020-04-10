8729af802dd6b2f90b717966ab745f4a819b804a
Marcin Chocilowicz <m.chocilowicz@gmail.com>
2020-04-10 22:19:51 +0200

    enable security

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './components/app/app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginComponent } from './pages/login/login.component';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { UserComponent } from './pages/user/user.component';
-import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './guards/auth.guard';
+import {AppComponent} from './components/app/app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {LoginComponent} from './pages/login/login.component';
+import {HTTP_INTERCEPTORS, HttpClient, HttpClientModule} from '@angular/common/http';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {UserComponent} from './pages/user/user.component';
+import {RouterModule, Routes} from '@angular/router';
+import {AuthGuard} from './guards/auth.guard';
 import {
     MatButtonModule,
     MatCheckboxModule,
@@ -26,37 +26,36 @@ import {
     MatSortModule,
     MatTableModule
 } from '@angular/material';
-import { CityComponent } from './pages/city/city.component';
-import { NavComponent } from './components/nav/nav.component';
-import { LoginService } from "./services/login.service";
-import { CityService } from "./services/city.service";
-import { UserService } from "./services/user.service";
-import { HttpService } from "./services/http.service";
-import { TradingPointComponent } from "./pages/trading-point/trading-point.component";
-import { TradingPointService } from "./pages/trading-point/trading-point-service.service";
-import { TradingPointViewComponent } from './pages/trading-point/trading-point-view/trading-point-view.component';
-import { NgoTypeComponent } from './pages/ngo-type/ngo-type.component';
-import { TradingPointTypeComponent } from './pages/trading-point-type/trading-point-type.component';
-import { SimpleDialogComponent } from './components/simple-dialog/simple-dialog.component';
-import { NgoComponent } from "./pages/ngo/ngo.component";
-import { StatsComponent } from "./pages/stats/stats.component";
-import { ConfigurationComponent } from "./pages/configuration/configuration.component";
-import { FileUploadDialogComponent } from './components/file-upload-dialog/file-upload-dialog.component';
-import { NgoViewComponent } from './pages/ngo/ngo-view/ngo-view.component';
-import { NgoService } from "./pages/ngo/ngo.service";
-import { ConfigurationService } from "./pages/configuration/configuration.service";
-import { OpinionService } from "./services/opinion.service";
-import { StatusService } from "./services/status.service";
-import { UserViewComponent } from './pages/user/user-view/user-view.component';
-import { MatRadioModule } from "@angular/material/radio";
-import { TransactionComponent } from './pages/transaction/transaction.component';
-import { TransactionViewComponent } from './pages/transaction/transaction-view/transaction-view.component';
-import { TransactionService } from "./services/transaction.service";
+import {NavComponent} from './components/nav/nav.component';
+import {LoginService} from "./services/login.service";
+import {CityService} from "./services/city.service";
+import {UserService} from "./services/user.service";
+import {HttpService} from "./services/http.service";
+import {TradingPointComponent} from "./pages/trading-point/trading-point.component";
+import {TradingPointService} from "./pages/trading-point/trading-point-service.service";
+import {TradingPointViewComponent} from './pages/trading-point/trading-point-view/trading-point-view.component';
+import {SimpleDialogComponent} from './components/simple-dialog/simple-dialog.component';
+import {NgoComponent} from "./pages/ngo/ngo.component";
+import {StatsComponent} from "./pages/stats/stats.component";
+import {ConfigurationComponent} from "./pages/configuration/configuration.component";
+import {FileUploadDialogComponent} from './components/file-upload-dialog/file-upload-dialog.component';
+import {NgoViewComponent} from './pages/ngo/ngo-view/ngo-view.component';
+import {NgoService} from "./pages/ngo/ngo.service";
+import {ConfigurationService} from "./pages/configuration/configuration.service";
+import {OpinionService} from "./services/opinion.service";
+import {StatusService} from "./services/status.service";
+import {UserViewComponent} from './pages/user/user-view/user-view.component';
+import {MatRadioModule} from "@angular/material/radio";
+import {TransactionComponent} from './pages/transaction/transaction.component';
+import {TransactionViewComponent} from './pages/transaction/transaction-view/transaction-view.component';
+import {TransactionService} from "./services/transaction.service";
 import {
     TradingPointSettlementComponent,
     TradingPointSettlementService
 } from './pages/trading-point-settlement/trading-point-settlement.component';
-import { NgoSettlementComponent, NgoSettlementService } from './pages/ngo-settlement/ngo-settlement.component';
+import {NgoSettlementComponent, NgoSettlementService} from './pages/ngo-settlement/ngo-settlement.component';
+import {ReportsComponent} from './pages/reports/reports.component';
+import {AuthInterceptor} from "./interceptors/auth.interceptor";
 
 export function HttpLoaderFactory(http: HttpClient) {
     return new TranslateHttpLoader(http);
@@ -75,21 +74,6 @@ export const appRoutes: Routes = [
     {
         path: 'user/:ID',
         component: UserViewComponent,
-        canActivate: [AuthGuard]
-    },
-    {
-        path: 'city',
-        component: CityComponent,
-        canActivate: [AuthGuard]
-    },
-    {
-        path: 'ngo-type',
-        component: NgoTypeComponent,
-        canActivate: [AuthGuard]
-    },
-    {
-        path: 'trading-point-type',
-        component: TradingPointTypeComponent,
         canActivate: [AuthGuard]
     },
     {
@@ -160,12 +144,9 @@ export const appRoutes: Routes = [
         AppComponent,
         LoginComponent,
         UserComponent,
-        CityComponent,
         NavComponent,
         TradingPointComponent,
         TradingPointViewComponent,
-        NgoTypeComponent,
-        TradingPointTypeComponent,
         SimpleDialogComponent,
         NgoComponent,
         StatsComponent,
@@ -176,7 +157,8 @@ export const appRoutes: Routes = [
         TransactionComponent,
         TransactionViewComponent,
         TradingPointSettlementComponent,
-        NgoSettlementComponent
+        NgoSettlementComponent,
+        ReportsComponent
     ],
     imports: [
         BrowserModule,
@@ -226,7 +208,8 @@ export const appRoutes: Routes = [
         StatusService,
         TransactionService,
         TradingPointSettlementService,
-        NgoSettlementService
+        NgoSettlementService,
+        {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
     ],
     entryComponents: [
         FileUploadDialogComponent,
